68566e284e618e9bd23e37a372dd6894bffd3b13
Thrauk <stoicath@gmail.com>
2021-12-15 23:47:50 +0200

    last assigment done

---
+++ build/CMakeFiles/myproject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myproject.dir/src/infantry.cpp.o"
+  "CMakeFiles/myproject.dir/src/main.cpp.o"
+  "CMakeFiles/myproject.dir/src/navy.cpp.o"
+  "CMakeFiles/myproject.dir/src/soldier.cpp.o"
+  "myproject"
+  "myproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
